a3ba60abf06e48a7866baf6e6b376b55d45dfccd
Francesco Buonocore <francibono@gmail.com>
2016-06-01 16:34:03 +0200

    Initial commit

---
+++ common/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>TIM</title>    
+    <link rel="stylesheet" href="css/style.css" />
+    
+</head>
+<body>
+
+    
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<?php
+
+include_once('common/header.php');
+
+echo "hello world";
+?>
+</head>
+<body>
+<?php include("common/footer.php"); ?>
+</body>
+</html>
