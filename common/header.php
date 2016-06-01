ec858e58e0924c252374172d61d4f985031b8023
Francesco Buonocore <francibono@gmail.com>
2016-06-01 17:11:58 +0200

    navbars and some magic

--- common/header.php
+++ common/header.php
@@ -1,6 +1,11 @@
 
-    <link rel="stylesheet" href="/css/style.css" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+
     <!-- Bootstrap -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <!--Custom Style-->
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
 
     
--- index.php
+++ index.php
@@ -3,12 +3,14 @@
 <head>
 <?php
     include_once('common/header.php');
-    include_once('common/navbar.php');
 ?>
 </head>
 <body>
-
-<div class="well">hello world!</div>
+<?php include_once('common/navbar_top.php'); ?>
+<div class="text-center logo_header">
+    <img src="img/TIM_logo_2016.png" alt="TIM_logo">
+</div>
+<?php include_once('common/navbar.php'); ?>
 
 
 <?php include("common/footer.php"); ?>
