63a3cc278d883d2631cc21721e95a68067bf6c4d
Francesco Buonocore <francibono@gmail.com>
2016-06-21 09:59:32 +0200

    footer

--- WEBSITE/includes/footer.php
+++ WEBSITE/includes/footer.php
@@ -1,7 +1,11 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 ?>
-<p> this is the footer</p>
+
+<div class="container">
+    <p class="text-muted">foooooter things</p>
+</div>
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
     <!-- Include all compiled plugins (below), or include individual files as needed -->
