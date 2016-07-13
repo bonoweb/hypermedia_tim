966870b20ddbdd1dee46ceca16a3b61a4884a381
Isiago <isabella.agosti@mail.polimi.it>
2016-07-13 18:15:26 +0200

    dave e ste in home

--- WEBSITE/js/scripts.js
+++ WEBSITE/js/scripts.js
@@ -20,6 +20,7 @@ function home(data) {
         r+= '</div>';
      r+= "</div>\n</div>";
      
+    r+='<img src="img/thumb_IMG_1607_1024.jpg">';
     return r;
 }
 
@@ -107,28 +108,80 @@ function ass_servizi_1(data)
     var r="";
     var count=1;
     var primo=1;
-    data.forEach(function(d){
     r+='<div class="col-md-3">\n';
     r+='<div class="thumbnail">\n';
     r+='<div class="caption">\n';
-        data.forEach(function(dt){
-            if(dt['id']==count){
-                if(primo==1){
-                    r+='<h2 style = "color:red;  text-align:center;">'+dt['titolo']+ '<br><br>'+ '</h2>\n';
-                    primo++;
-                }
-            else{
-                r+='<h4 style = "color:blue;">'+dt['titolo']+ ' &raquo;' + '</h4>\n';
-            }
-        }
-        });
-        count++;
-        primo=1;
-        r+='</div>';
-        r+='</div>';
-        r+='</div>';
+    data.forEach(function(d){
+       if(d['id']==count){
+           if(primo==1){
+               r+='<h2 style = "color:red;  text-align:center;">'+d['titolo']+ '<br><br>'+ '</h2>\n';
+               primo++;
+           }
+           else{
+               r+='<h4 style = "color:blue;">'+d['titolo']+ ' &raquo;' + '</h4>\n';
+           }
+       }
     });
-    
+    r+='</div>';
+    r+='</div>';
+    r+='</div>';
+    count++;
+    primo=1;
+    r+='<div class="col-md-3">\n';
+    r+='<div class="thumbnail">\n';
+    r+='<div class="caption">\n'
+    data.forEach(function(d){
+       if(d['id']==count){
+           if(primo==1){
+               r+='<h2 style = "color:red;  text-align:center;">'+d['titolo']+ '<br><br>' + '</h2>\n';
+               primo++;
+           }
+           else{
+               r+='<h4 style = "color:blue;">'+d['titolo']+ ' &raquo;' + '</h4>\n';
+           }
+       }
+    });
+    r+='</div>';
+    r+='</div>';
+    r+='</div>';
+    count++;
+    primo=1;
+    r+='<div class="col-md-3">\n';
+    r+='<div class="thumbnail">\n';
+    r+='<div class="caption">\n'
+    data.forEach(function(d){
+       if(d['id']==count){
+           if(primo==1){
+               r+='<h2 style = "color:red;  text-align:center;">'+d['titolo']+ '</h2>\n';
+               primo++;
+           }
+           else{
+               r+='<h4 style = "color:blue;">'+d['titolo']+ ' &raquo;' + '</h4>\n';
+           }
+       }
+    });
+    r+='</div>';
+    r+='</div>';
+    r+='</div>';
+    count++;
+    primo=1;
+    r+='<div class="col-md-3">\n';
+    r+='<div class="thumbnail">\n';
+    r+='<div class="caption">\n'
+    data.forEach(function(d){
+       if(d['id']==count){
+           if(primo==1){
+               r+='<h2 style = "color:red;  text-align:center;">'+d['titolo'] +'</h2>\n';
+               primo++;
+           }
+           else{
+               r+='<h4 style = "color:blue;">'+d['titolo']+ ' &raquo;' + '</h4>\n';
+           }
+       }
+    });
+    r+='</div>';
+    r+='</div>';
+    r+='</div>';
     return r;
 }
 
@@ -175,30 +228,19 @@ function vprod(data)
 
 function innovazione(data)
 {
-    var r="";
-    var count=1;
+     var r="";
     r+="<div class='container'>\n<div class=\"row\">";
-    r+='<h1 style = "color:red; font-weight:bold; text-align:center; font-size:3.5em;">' + 'VIVERE L\'INNOVAZIONE' + '</h1><br>\n';
     data.forEach(function(d){
         r+='<div class="thumbnail">\n';
         r+='<div class="caption">\n';
         r+='<div class="row">';
-        r+='<div class="col-md-1">';
-        r+='<img style="min-width:1140px; max-height:450px;" src="img/'+d['immagine']+'" alt="Immagine'+d['immagine']+'">' ;
+        r+='<div class="col-md-6">';
+        r+='<img style="max-width:100%;" src="img/'+d['immagine']+'" alt="Immagine'+d['immagine']+'" class="img-responsive text-center">' ;
         r+='</div>';
-        if(count%2!=0){
-            r+='<div class="col-md-6">';
-            r+='<h2 style="color:white; text-align:left;">'+ '<br><br>' +d['titolo']+'</h2>\n';
-            r+='<h4 style="color:white; text-align:left;">'+d['testo']+'</h4>\n';
-            r+='</div>';
-        }
-        else{
-            r+='<div class="col-md-6">';
-            r+='<h2 style="color:white; text-align:right;">'+ '<br><br>' +d['titolo']+'</h2>\n';
-            r+='<h4 style="color:white; text-align:right;">'+d['testo']+'</h4>\n';
-            r+='</div>';
-        }
-        count++;
+        r+='<div class="col-md-6">';
+        r+='<h2>'+d['titolo']+'</h2>\n';
+        r+='<h4>'+d['testo']+'</h4>\n';
+        r+='</div>';
         r+='</div>';
         r+='</div>';
         r+='</div>';
